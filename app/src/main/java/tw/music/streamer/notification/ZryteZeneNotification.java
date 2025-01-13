5869bb938c49f5e41f9f0924fd96824756ab8f09
AdityaKapal362 <adityakapal362@gmail.com>
2025-01-13 14:12:16 +0000

    fix typo on intent part 2

--- app/src/main/java/tw/music/streamer/notification/ZryteZeneNotification.java
+++ app/src/main/java/tw/music/streamer/notification/ZryteZeneNotification.java
@@ -120,12 +120,12 @@ public class ZryteZeneNotification {
     	PendingIntent previousPendingIntent = PendingIntent.getService(a, 1, previousIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
 
     	Intent nextIntent = new Intent(a, ZryteZenePlay.class);
-    	nextIntent.setAction(ZryteZenePlay.ACTION_BROADCAST;
+    	nextIntent.setAction(ZryteZenePlay.ACTION_BROADCAST);
 		nextIntent.putExtra("action", "forward");
     	PendingIntent nextPendingIntent = PendingIntent.getService(a, 3, nextIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
 
 		Intent stopIntent = new Intent(a, ZryteZenePlay.class);
-    	stopIntent.setAction(ZryteZenePlay.ACTION_BROADCAST;
+    	stopIntent.setAction(ZryteZenePlay.ACTION_BROADCAST);
 		stopIntent.putExtra("action", "stop");
     	PendingIntent nextPendingIntent = PendingIntent.getService(a, 2, stopIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
 
