afc83d7420debfdfd9c27fc43a4999994a27b197
AdityaKapal362 <adityakapal362@gmail.com>
2025-01-14 03:58:30 +0000

    pleasejiaojrjeirjoibqewtb

--- app/src/main/java/tw/music/streamer/MainActivity2.java
+++ app/src/main/java/tw/music/streamer/MainActivity2.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity2 extends Activity {
+public class MainActivity2 extends AppCompatActivity {
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
