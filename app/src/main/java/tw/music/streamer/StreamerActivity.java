c09908ffe107282e7e497daeaed2b1f66479a382
AdityaKapal362 <adityakapal362@gmail.com>
2025-01-16 13:41:11 +0000

    disabling initializeLogic for testing since it gives error on start of the activity

--- app/src/main/java/tw/music/streamer/StreamerActivity.java
+++ app/src/main/java/tw/music/streamer/StreamerActivity.java
@@ -324,12 +324,7 @@ public class StreamerActivity extends AppCompatActivity {
         setContentView(R.layout.streamer);
         com.google.firebase.FirebaseApp.initializeApp(this);
         initialize(_savedInstanceState);
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
-                || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
-        } else {
-            initializeLogic();
-        }
+        //initializeLogic();
     }
 
     @Override
