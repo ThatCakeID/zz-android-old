14204b49ee4c38e16785e9b93d6877f84fb9d55d
AdityaKapal362 <adityakapal362@gmail.com>
2025-01-22 05:24:19 +0700

    added random song adaptor

--- app/src/main/java/tw/music/streamer/StreamingActivity.java
+++ app/src/main/java/tw/music/streamer/StreamingActivity.java
@@ -28,12 +28,14 @@ import java.util.HashMap;
 import tw.music.streamer.adaptor.ZZSong;
 import tw.music.streamer.adaptor.ZryteZeneAdaptor;
 import tw.music.streamer.adapter.ZZSongAdapter;
+import tw.music.streamer.adapter.ZZRandomSongAdapter;
 import tw.music.streamer.service.ZryteZenePlay;
 
 public class StreamingActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
     private DatabaseReference db_song;
+    private DatabaseReference db_drsong;
     private StorageReference fs_quota;
     private StorageReference fs_music;
     private StorageReference fs_icon;
@@ -49,7 +51,9 @@ public class StreamingActivity extends AppCompatActivity {
     private ImageView user_icon;
 
     private ArrayList<ZZSong> zz_songs;
+    private ArrayList<ZZSong> zz_songs2;
 
+    private ZZRandomSongAdapter ra_songs;
     private ZZSongAdapter ar_songs;
 
     @Override
@@ -65,6 +69,7 @@ public class StreamingActivity extends AppCompatActivity {
 
     private void initVariables(Context a) {
         zz_songs = new ArrayList<>();
+        ra_songs = new ZZRandomSongAdapter(zz_songs2);
         ar_songs = new ZZSongAdapter(zz_songs);
         lm1 = new LinearLayoutManager(a, LinearLayoutManager.HORIZONTAL, false);
         lm2 = new GridLayoutManager(a, 2);
@@ -73,6 +78,7 @@ public class StreamingActivity extends AppCompatActivity {
     private void initFirebase() {
         auth = FirebaseAuth.getInstance();
         db_song = FirebaseDatabase.getInstance().getReference("zrytezene/songs");
+        db_drsong = FirebaseDatabase.getInstance().getReference("zrytezene/daily-random-songs");
         //db_admin = FirebaseDatabase.getInstance().getReference("profile/admins");
         //db_comment = FirebaseDatabase.getInstance().getReference("zrytezene/comments");
         //db_profile = FirebaseDatabase.getInstance().getReference("profile/text");
@@ -99,6 +105,18 @@ public class StreamingActivity extends AppCompatActivity {
                 }
             } else {
                 // error: task.getException().getMessage()
+            }
+        });
+
+        db_drsong.get().addOnCompleteListener(a -> {
+            if (a.isSuccessful()) {
+                DataSnapshot b = a.getResult();
+                if (b.exists()) {
+                    for (DataSnapshot c : b.getChildren()) {
+                        zz_songs2.add(new ZZSong(c));
+                    }
+                    ra_songs.notifyDataSetChanged();
+                }
             }
         });
 
@@ -138,6 +156,7 @@ public class StreamingActivity extends AppCompatActivity {
 
     private void initLogic(Context a) {
 		rv_random_songs.setLayoutManager(lm1);
+        rv_random_songs.setAdapter(ra_songs);
         rv_songs.setLayoutManager(lm2);
         rv_songs.setAdapter(ar_songs);
     }
