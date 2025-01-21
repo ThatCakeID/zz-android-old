e42a06cf9c6bcb866aa44dd07fd1da728a143c0b
AdityaKapal362 <adityakapal362@gmail.com>
2025-01-22 02:01:51 +0700

    added ZZSong for songs adapter

---
+++ app/src/main/java/tw/music/streamer/adaptor/ZZSong.java
@@ -0,0 +1,21 @@
+package tw.music.streamer.adaptor;
+
+import com.google.firebase.database.DataSnapshot;
+
+public class ZZSong {
+
+    public String key, url_song, url_icon, url_cover, uploader_uid, uploader_name, song_name, song_artist;
+    public boolean playing;
+
+    public ZZSong(DataSnapshot a) {
+        playing = false;
+        key = a.getKey();
+        url_song = a.child("song").getValue(String.class);
+        url_icon = a.child("icon").getValue(String.class);
+        url_cover = a.child("cover").getValue(String.class);
+        uploader_uid = a.child("uid").getValue(String.class);
+        uploader_name = a.child("uploader").getValue(String.class);
+        song_name = a.child("title").getValue(String.class);
+        song_artist = a.child("artist").getValue(String.class);
+    }
+}
