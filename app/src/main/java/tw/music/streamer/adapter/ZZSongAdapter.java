719783354adca0efe67ea5caca992f980d113c33
AdityaKapal362 <adityakapal362@gmail.com>
2025-01-22 02:22:25 +0700

    fix cover typo

--- app/src/main/java/tw/music/streamer/adapter/ZZSongAdapter.java
+++ app/src/main/java/tw/music/streamer/adapter/ZZSongAdapter.java
@@ -33,7 +33,7 @@ public class ZZSongAdapter extends RecyclerView.Adapter<ZZSongAdapter.ZZViewHold
     public void onBindViewHolder(@NonNull ZZViewHolder h, int p) {
         h.title.setText(data.get(p).song_name);
         h.artist.setText(data.get(p).song_artist);
-        Glide.with(getApplicationContext()).load(data.get(p).url_cover).into(profile_icon);
+        Glide.with(getApplicationContext()).load(data.get(p).url_cover).into(h.cover);
     }
 
     @Override
