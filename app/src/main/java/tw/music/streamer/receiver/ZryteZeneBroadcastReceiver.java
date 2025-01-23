816644aebeab84b9b02ff835d396e11ff13b0dba
AdityaKapal362 <adityakapal362@gmail.com>
2025-01-23 12:53:47 +0700

    added send broadcast to ZZPlay

--- app/src/main/java/tw/music/streamer/receiver/ZryteZeneBroadcastReceiver.java
+++ app/src/main/java/tw/music/streamer/receiver/ZryteZeneBroadcastReceiver.java
@@ -3,46 +3,37 @@ package tw.music.streamer.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
-import tw.music.streamer.service.ZryteZenePlay;
+import tw.music.streamer.adaptor.ZryteZeneAdaptor;
 
 public class ZryteZeneBroadcastReceiver extends BroadcastReceiver {
 
-    public static final String PLAY = "tw.music.streamer.ACTION_PLAY";
-    public static final String PAUSE = "tw.music.streamer.ACTION_PAUSE";
-    public static final String NEXT = "tw.music.streamer.ACTION_NEXT";
-    public static final String PREVIOUS = "tw.music.streamer.ACTION_PREVIOUS";
+    public static final String PLAY = "ZZ_PLAY";
+    public static final String PAUSE = "ZZ_PAUSE";
+    public static final String SKIP = "ZZ_SKIP";
+    public static final String PREVIOUS = "ZZ_PREVIOUS";
+
+    public ZryteZeneAdaptor zz;
 
     @Override
-    public void onReceive(Context a, Intent b) {
-        if (b == null || b.getAction() == null) return;
-
-        String c = b.getAction();
-        Intent d = new Intent(a, ZryteZenePlay.class);
-
-        switch (c) {
-            case PLAY:
-                d.putExtra("action", "play");
-                break;
-
-            case PAUSE:
-                d.putExtra("action", "pause");
-                break;
-
-            case NEXT:
-                d.putExtra("action", "next");
-                break;
-
-            case PREVIOUS:
-                d.putExtra("action", "previous");
-                break;
-
-            default:
-                Log.w("MusicBroadcastReceiver", "Unknown action: " + c);
-                return;
+    public void onReceive(Context context, Intent intent) {
+        zz = new ZryteZeneAdaptor(context);
+        String action = intent.getAction();
+        if (action != null) {
+            switch (action) {
+                case PLAY:
+                    zz.requestAction("resume");
+                    break;
+                case PAUSE:
+                    zz.requestAction("pause");
+                    break;
+                case SKIP:
+                    zz.requestAction("skip");
+                    break;
+                case PREVIOUS:
+                    zz.requestAction("previous");
+                    break;
+            }
         }
-
-        a.startService(d);
     }
 }
