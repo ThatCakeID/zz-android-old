72bcfb84a28029901886210abccc252308b0596d
AdityaKapal362 <adityakapal362@gmail.com>
2025-01-24 15:48:40 +0700

    added asynctask to download the song

--- app/src/main/java/tw/music/streamer/adaptor/ZryteZeneSongsManager.java
+++ app/src/main/java/tw/music/streamer/adaptor/ZryteZeneSongsManager.java
@@ -4,28 +4,18 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.FileDownloadTask;
 
+import android.os.AsyncTask;
 import android.content.Context;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 public class ZryteZeneSongsManager {
 
-    public static void downloadSong(String fileUrl, String localPath, DownloadCallback callback) {
-        StorageReference storageRef = FirebaseStorage.getInstance().getReferenceFromUrl(fileUrl);
-        File localFile = new File(localPath);
-        try {
-            if (!localFile.exists()) {
-                localFile.createNewFile();
-            }
-            storageRef.getFile(localFile).addOnSuccessListener(taskSnapshot -> {
-                callback.onSuccess(localPath);
-            }).addOnFailureListener(exception -> {
-                callback.onFailure(exception.getMessage());
-            });
-        } catch (IOException e) {
-            callback.onFailure(e.getMessage());
-        }
+    public static void download(Context a, String b, String c) {
+        new DownloadSong(a, ZZSong.PATH + b, c).execute();
     }
 
     public static boolean check(Context context, String key) {
@@ -35,8 +25,51 @@ public class ZryteZeneSongsManager {
         return song.exists() && song.isFile();
     }
 
-    public static interface DownloadCallback {
-        void onSuccess(String message);
-        void onFailure(String errorMessage);
+    public static class DownloadSong extends AsyncTask<Void, Void, Boolean> {
+        private Context context;
+        private String firebaseUrl;
+        private String fileName;
+
+        public FirebaseDownloadTask(Context a, String b, String c) {
+            context = a;
+            firebaseUrl = b;
+            fileName = c;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            try {
+                File songsCacheDir = new File(context.getFilesDir(), "songs-cache");
+                if (!songsCacheDir.exists() && !songsCacheDir.mkdirs()) {
+                    return false;
+                }
+                File songFile = new File(songsCacheDir, fileName);
+                StorageReference storageRef = FirebaseStorage.getInstance().getReferenceFromUrl(firebaseUrl);
+                File tempFile = File.createTempFile("zz-song", null);
+                storageRef.getFile(tempFile).addOnSuccessListener(taskSnapshot -> {
+                    try {
+                        InputStream input = context.getContentResolver().openInputStream(android.net.Uri.fromFile(tempFile));
+                        FileOutputStream output = new FileOutputStream(songFile);
+                        byte[] buffer = new byte[4096];
+                        int length;
+                        while ((length = input.read(buffer)) > 0) {
+                            output.write(buffer, 0, length);
+                        }
+                        input.close();
+                        output.close();
+                        tempFile.delete();
+                    } catch (Exception e) {
+                    }
+                });
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+        }
     }
+
 }
