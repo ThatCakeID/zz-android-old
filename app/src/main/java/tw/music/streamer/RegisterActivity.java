5d96c9138a6b27a692af297b4bc0bc93f339a0c8
AdityaKapal362 <adityakapal362@gmail.com>
2025-01-30 22:35:57 +0700

    imports

--- app/src/main/java/tw/music/streamer/RegisterActivity.java
+++ app/src/main/java/tw/music/streamer/RegisterActivity.java
@@ -2,8 +2,19 @@ package tw.music.streamer;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.text.TextUtils;
+import android.content.Intent;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
